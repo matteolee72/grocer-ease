cbc0d7c34a81e40e67641445869ea6de4561b8c9
matteolee72 <matthias.lee.dong.en@gmail.com>
2023-03-05 00:52:41 +0800

    main test change

--- app/src/main/java/com/example/grocerease/MainActivity.java
+++ app/src/main/java/com/example/grocerease/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        //hello I am making a change
     }
 
 }
