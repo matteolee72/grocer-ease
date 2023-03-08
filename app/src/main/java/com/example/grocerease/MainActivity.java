3e858151b9348c0ec0c7639201c74e4f7a8faff4
jbongbong <jbongryan@gmail.com>
2023-03-08 19:40:34 +0800

    set up firebase things within Android Studio

--- app/src/main/java/com/example/grocerease/MainActivity.java
+++ app/src/main/java/com/example/grocerease/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.grocerease;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import com.example.grocerease.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.EventListener;
 
+public class MainActivity extends AppCompatActivity {
+    TextView textViewMsg;
+
+    final String node = "current_msg";
+    DatabaseReference mRootDatabaseRef;
+    DatabaseReference mNodeRef;
     private ActivityMainBinding binding;
 
     @Override
@@ -22,6 +36,23 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+        textViewMsg = findViewById(R.id.textViewMesg);
+        mRootDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mNodeRef = mRootDatabaseRef.child(node);
+
+        mNodeRef.addValueEventListener(new ValueEventListener(){
+            //Update what is being changed
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+                String change = snapshot.getValue(String.class);
+                textViewMsg.setText(change);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error){
+            }
+        });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
