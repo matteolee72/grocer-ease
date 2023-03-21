5e00edbecdd447ae2e578cfd61583d9db26ba9c2
cephasyeo <85869347+cephasyeo@users.noreply.github.com>
2023-03-21 17:04:56 +0800

    not working integration. need to find a way to get the output of the qr code scan to the input of the app

---
+++ app/src/main/java/com/example/grocerease/CaptureAct.java
@@ -0,0 +1,7 @@
+package com.example.grocerease;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+
+}
--- app/src/main/java/com/example/grocerease/ScanActivity.java
+++ app/src/main/java/com/example/grocerease/ScanActivity.java
@@ -1,7 +1,13 @@
 package com.example.grocerease;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.DefaultLifecycleObserver;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,11 +16,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
 
 public class ScanActivity extends AppCompatActivity {
 
     EditText debug_barcodenumber;
     Button debug_scanbutton;
+    String barcodeNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +32,8 @@ public class ScanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scan);
 
         Intent singleItemToScan = getIntent();
-        String barcodeNum1 = singleItemToScan.getStringExtra(MainActivity.FIRSTBARCODEKEY);
-        Log.i("SingleItemAnalyse", "Barcode received: "+ barcodeNum1);
+        String firstBarcode = singleItemToScan.getStringExtra(MainActivity.FIRSTBARCODEKEY);
+        Log.i("SingleItemAnalyse", "Barcode received: "+ firstBarcode);
 
         debug_barcodenumber = findViewById(R.id.editText_debug_code);
         debug_scanbutton = findViewById(R.id.button_scan_activity);
@@ -33,27 +43,59 @@ public class ScanActivity extends AppCompatActivity {
         debug_scanbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String barcodeNum = debug_barcodenumber.getText().toString();
+                scanCode();
                 Log.i("ScanActivity", "Barcode Number: " + barcodeNum);
 
+                // If the barcodeNum is not empty
+                // ie there already a barcode stored in it due to the barcode scanning earlier
                 if (!barcodeNum.isEmpty()) {
-                    if (barcodeNum1 == null){
+                    // We check if the stored first barcode from the 1 item page is passed into
+                    // this function.
+                    if (firstBarcode == null){
                         Intent intent = new Intent(ScanActivity.this, SingleItemAnalyze.class);
                         intent.putExtra(MainActivity.FIRSTBARCODEKEY, barcodeNum); //Using putExtra, implement mPreferences next
                         startActivity(intent);
                     }
                     else{
+                        // If firstBarcode contains something, then we assume that we are now
+                        // scanning the second barcode. So we run this block of code.
                         Intent intent = new Intent(ScanActivity.this, TwoItemCompare.class);
-                        intent.putExtra(MainActivity.FIRSTBARCODEKEY, barcodeNum1); //Using putExtra, implement mPreferences next
+                        intent.putExtra(MainActivity.FIRSTBARCODEKEY, firstBarcode); //Using putExtra, implement mPreferences next
                         intent.putExtra(MainActivity.SECONDBARCODEKEY, barcodeNum);
                         startActivity(intent);
                     }
                 }
-                else {
-                    Toast.makeText(ScanActivity.this, "Enter a barcode number", Toast.LENGTH_LONG).show();
-                }
 
             }
+            // creating new scancode class to setup camera
+            // stuff here not necessary, can change
+            public void scanCode() {
+                ScanOptions options = new ScanOptions();
+                options.setPrompt("Volume up to flash on");
+                options.setBeepEnabled(true);
+                options.setOrientationLocked(true);
+                options.setCaptureActivity(CaptureAct.class);
+                barLauncher.launch(options);
+            }
+
+            // TO BE TESTED
+            // able to retrieve data from barcode
+            // if barcode is correct, we return result
+            ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result->
+            {
+                if(result.getContents() != null)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ScanActivity.this);
+                    builder.setTitle("Result");
+                    builder.setMessage(result.getContents());
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    }).show();
+                }
+            });
         });
     }
 }
