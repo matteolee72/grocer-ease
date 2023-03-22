c2e00e4d456e7f67ee0b9b27f9017755041e863f
cephasyeo <85869347+cephasyeo@users.noreply.github.com>
2023-03-22 18:11:25 +0800

    ScanActivity Fixed

--- app/src/main/java/com/example/grocerease/ScanActivity.java
+++ app/src/main/java/com/example/grocerease/ScanActivity.java
@@ -26,6 +26,49 @@ public class ScanActivity extends AppCompatActivity {
     Button debug_scanbutton;
     String barcodeNum;
 
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() != null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(ScanActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            Log.d("scanactivity", result.getContents());
+            Intent singleItemToScan = getIntent();
+            String firstBarcode = singleItemToScan.getStringExtra(MainActivity.FIRSTBARCODEKEY);
+
+            barcodeNum = result.getContents();
+
+            if (firstBarcode == null){
+                Intent intent = new Intent(ScanActivity.this, SingleItemAnalyze.class);
+                intent.putExtra(MainActivity.FIRSTBARCODEKEY, barcodeNum); //Using putExtra, implement mPreferences next
+                startActivity(intent);
+            }
+            else{
+                // If firstBarcode contains something, then we assume that we are now
+                // scanning the second barcode. So we run this block of code.
+                Intent intent = new Intent(ScanActivity.this, TwoItemCompare.class);
+                intent.putExtra(MainActivity.FIRSTBARCODEKEY, firstBarcode); //Using putExtra, implement mPreferences next
+                intent.putExtra(MainActivity.SECONDBARCODEKEY, barcodeNum);
+                startActivity(intent);
+            }
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+
+    public void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +81,7 @@ public class ScanActivity extends AppCompatActivity {
         debug_barcodenumber = findViewById(R.id.editText_debug_code);
         debug_scanbutton = findViewById(R.id.button_scan_activity);
 
+        scanCode();
 
 
         debug_scanbutton.setOnClickListener(new View.OnClickListener() {
@@ -48,7 +92,7 @@ public class ScanActivity extends AppCompatActivity {
 
                 // If the barcodeNum is not empty
                 // ie there already a barcode stored in it due to the barcode scanning earlier
-                if (!barcodeNum.isEmpty()) {
+                if (true) {
                     // We check if the stored first barcode from the 1 item page is passed into
                     // this function.
                     if (firstBarcode == null){
@@ -69,33 +113,12 @@ public class ScanActivity extends AppCompatActivity {
             }
             // creating new scancode class to setup camera
             // stuff here not necessary, can change
-            public void scanCode() {
-                ScanOptions options = new ScanOptions();
-                options.setPrompt("Volume up to flash on");
-                options.setBeepEnabled(true);
-                options.setOrientationLocked(true);
-                options.setCaptureActivity(CaptureAct.class);
-                barLauncher.launch(options);
-            }
+
 
             // TO BE TESTED
             // able to retrieve data from barcode
             // if barcode is correct, we return result
-            ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result->
-            {
-                if(result.getContents() != null)
-                {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(ScanActivity.this);
-                    builder.setTitle("Result");
-                    builder.setMessage(result.getContents());
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            dialogInterface.dismiss();
-                        }
-                    }).show();
-                }
-            });
+
         });
     }
 }
